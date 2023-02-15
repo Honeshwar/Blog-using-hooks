fc3092d11318d5109e66b39fc0f35066b6c4311e
Honeshwar Thakur <st566654@gmail.com>
2023-02-15 19:26:07 +0530

    install 3rd party module react-router-dom and using it creater routers{browserRouter,Routes,Route}

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
-import db from '../firebase';
-
+// import db from '../firebase';
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Navbar from './Navbar';
+import Home from './Home';
+import PostDetail from './PostDetail';
+import CreatePost from "./CreatePost";
 function App() {
-  let data=[];
-  db.collection('blog')
-  .onSnapshot((snapshot)=>{
-    snapshot.docs.map((doc)=>{
-    console.log(doc.data());
-    // data.push(doc.data().title);
-    });
-  })
+ 
+
   return (
     <div className="App">
-    <h1>Blojkhghfdfg</h1>{data[0] }
-    </div>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/post/:postId" element={<PostDetail/>}></Route>
+      <Route exact path="/createPost" element={<CreatePost/>}></Route>
+    </Routes>
+    </BrowserRouter>
+    </div> 
   );
 }
 
 export default App;
+// let data=[];
+// db.collection('blog')
+// .onSnapshot((snapshot)=>{
+//   snapshot.docs.map((doc)=>{
+//   console.log(doc.data());
+//   // data.push(doc.data().title);
+//   });
+// });
