41680d06f06e561d3cc7ffc992c111df6ca71e22
Honeshwar Thakur <st566654@gmail.com>
2023-02-15 19:29:38 +0530

    use Link component from react-router0dom library/module and create router reload free,(using anchor tag on change url(when click link) page get reload)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-  
+    <> 
     <div>Navbar</div>
-   
+    <ul>
+      <h1>c</h1>
+      <li><Link to='/'>Home</Link></li>
+      <li><Link to='/post/jhg'>Post</Link></li>
+      <li><Link to='/createPost'>Create Post</Link></li>
+<h3>difference between anchor tag(reload page) and link components(not reload page)</h3>
+<h1>anchor
+
+  
+</h1>
+      <li><a href='/'>Home</a></li>
+      <li><a href='/post/jhg'>Post</a></li>
+      <li><a href='/createPost'>Create Post</a></li>
+    </ul>
+   </>
   )
 }
