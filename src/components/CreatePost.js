2e472254084e388898292e4231dcb5fdbb0434fc
Honeshwar Thakur <st566654@gmail.com>
2023-02-16 13:24:30 +0530

    create a post form + use use state to get data from input not from submission of from (value go to payload)

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function CreatePost() {
+//use state hooks to get data from input
+const [title,setTitle] = useState('');
+const [subTitle,setSubTitle] = useState('');
+const [content,setContent] = useState('');
+
+function handleSubmission(e){
+  e.preventDefault();
+  console.log("title",title);
+  console.log("subtitle",subTitle);
+  console.log("content",content);
+}
+
   return (
-    <div>CreatePost</div>
+    <div className='create-post-form'>
+    <form onSubmit={handleSubmission}>
+      <div className='form-field'>
+      <label>Title</label>
+        <input value={title} onChange={(e)=>{setTitle(e.target.value);}} placeholder='Enter title'/>
+      </div>
+      <div className='form-field'>
+      <label>Sub Title</label>
+        <input value={subTitle} onChange={(e)=>{setSubTitle(e.target.value);}} placeholder='Enter Subtitle'/>
+      </div>
+      <div className='form-field'>
+      <label>Content</label>
+        <textarea value={content} onChange={(e)=>{setContent(e.target.value);}}placeholder='Enter your post'></textarea>
+      </div>
+
+      <button id='create-post-button'>Create Post</button>
+    </form>
+    </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 export default function Navbar() {
   return (
-    <> 
-    <div>Navbar</div>
-    <ul>
-      <h1>c</h1>
-      <li><Link to='/'>Home</Link></li>
-      <li><Link to='/post/jhg'>Post</Link></li>
-      <li><Link to='/createPost'>Create Post</Link></li>
-<h3>difference between anchor tag(reload page) and link components(not reload page)</h3>
-<h1>anchor
+    <div id='navbar'>
+      <img src='https://th.bing.com/th/id/OIP.4eNuH6v50eBsLABnFsZUrgHaE7?w=252&h=180&c=7&r=0&o=5&pid=1.7'/>
+      <ul >
 
-  
-</h1>
-      <li><a href='/'>Home</a></li>
-      <li><a href='/post/jhg'>Post</a></li>
-      <li><a href='/createPost'>Create Post</a></li>
-    </ul>
-   </>
-  )
+        <li><Link className='link' to='/'>Home</Link></li>
+        <li><Link className='link' to='/post/jhg'>Post Detail</Link></li>
+        <li><Link className='link' to='/createPost'>Create Post</Link></li>
+
+      </ul>
+    </div>
+  );
 }
